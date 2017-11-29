47af6e9bf46e28095e631ad03d5e0901a034b7ea
vindu <st46514@student.upce.cz>
2017-11-29 20:31:42 +0100

    ConsoleWriter

--- src/main/java/com/edu/rozvrhHodin/frontend/ConsoleWriter.java
+++ src/main/java/com/edu/rozvrhHodin/frontend/ConsoleWriter.java
@@ -1,7 +1,30 @@
 package com.edu.rozvrhHodin.frontend;
 
-public class ConsoleWriter {
+public final class ConsoleWriter {
     public ConsoleWriter(){
-        System.out.println("ahoj");
+
+    }
+    public static void menuWriter(){
+        System.out.println("-----Menu-----");
+        System.out.println("1 - Vytvoření studenta");
+        System.out.println("2 - Vytvoření předmětu");
+        System.out.println("3 - Zobrazení informací");
+        System.out.println("5 - Editace účtu studenta");
+        System.out.println("6 - Editace předmětu");
+        System.out.println("7 - Deaktivace studenta");
+        System.out.println("8 - Přihlášení/Odhlášení studentů z předmětu");
+        System.out.println("9 - Konec");
+    }
+    public static void informationWriter(){
+        System.out.println("1 - Zobrazení rozvrhu studenta");
+        System.out.println("2 - Zobrazení rozvrhu více studentů");
+        System.out.println("3 - Zobrazení volného času");
+        System.out.println("4 - Zobrazení přihlášených studentů na předmět");
+        System.out.println("5 - Výpis všech předmětů studenta");
+        System.out.println("6 - Výpis údajů konkrétního studenta");
+        System.out.println("7 - Výpis všech studentů");
+        System.out.println("8 - Výpis údajů konkrétního předmětu");
+        System.out.println("9 - Výpis všech předmětů");
+        System.out.println("0 - Zpět");
     }
 }
